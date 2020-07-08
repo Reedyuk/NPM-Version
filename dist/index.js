6171c88e32d94ce932fb2ff2bece5dd3bd03def9
Andrew Reed <andrew_reed@hotmail.com>
2020-07-08 22:34:16 +0100

    ncc up

--- dist/index.js
+++ dist/index.js
@@ -309,12 +309,12 @@ async function run() {
     const version = core.getInput("version");
     const package = core.getInput("package");
 
-    const child = exec("npm version "+version, (error, stdout, stderr) => {
+    const child = exec("npm version " + version + " --no-git-tag-version", (error, stdout, stderr) => {
         if(error != null) {
             core.setFailed(error);
         }
         if(stderr != null) {
-            core.setFailed(stderr);
+            console.log(stderr);
         }
         console.log(stdout);
     });
