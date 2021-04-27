2a9686b32f18822393678a2734e6613c74c8eb5f
Andrew Reed <andrew_reed@hotmail.com>
2021-04-27 19:03:19 +0100

    added default value

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const exec = require('child_process').exec;
 
 async function run() {
     const version = core.getInput("version");
-    const gitTagVersion = core.getInput("git-tag-version");
+    const gitTagVersion = core.getInput("git-tag-version") ?? false;
     const package = core.getInput("package").replace("package.json", "");
-
-    console.log("value of git-tag-version "+gitTagVersion);
 
     const child = exec("npm version " + version + " --prefix " + package + " " + (!gitTagVersion ? "--no-git-tag-version" : ""), (error, stdout, stderr) => {
         if(error != null) {
