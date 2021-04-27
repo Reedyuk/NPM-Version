7b8f5e72465712074e439a158205f89d8fa4d315
Andrew Reed <andrew_reed@hotmail.com>
2021-04-27 19:06:14 +0100

    Added fix for boolean

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const exec = require('child_process').exec;
 
 async function run() {
     const version = core.getInput("version");
-    const gitTagVersion = core.getInput("git-tag-version") ?? false;
+    const gitTagVersion = core.getInput("git-tag-version").toUpper() === 'true';
     const package = core.getInput("package").replace("package.json", "");
 
     const child = exec("npm version " + version + " --prefix " + package + " " + (!gitTagVersion ? "--no-git-tag-version" : ""), (error, stdout, stderr) => {
